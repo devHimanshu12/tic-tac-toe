315e57fe980f37cdf60c9067ef9726f1d0ab98a9
himanshu <shimanshu94@gmail.com>
2024-05-28 23:30:12 +0530

    working on game project

---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+export default function Player({initialName,symbol}) {
+    const [playerName, setPlayerName] = useState(initialName)
+    const [isEditing,setEditing] = useState(false)
+
+    function handleEdit(){
+        setEditing(editing => !editing)
+    }
+
+    function handleInput(event){
+        setPlayerName(event.target.value)
+    }
+
+    let player = <span className="player-name">{playerName}</span>
+    if(isEditing){
+        player = <input type="text" required value={playerName} onChange={handleInput}/>
+    }
+
+
+    return <li>
+        <span className="player">
+            {player}
+            <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+}
